27c795091ae814e549f7757bb114ecf3208943e6
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-10-09 11:58:29 +0200

    Make code more reliable (checks performed on boxable init), add comments

--- lib/box/acts_as_boxable.rb
+++ lib/box/acts_as_boxable.rb
@@ -38,10 +38,33 @@ module Box
     module ClassMethods
       def acts_as_boxable(options = {})
         class_eval do
-          cattr_accessor :boxable_config
-          self.boxable_config = Box::BoxableConfig.new(options) # Store boxable metadata in the instance
+          # @abstract Get metadata about the class Box parent
+          # @return Hash
+          def self.box_parent_meta
+            parent_association = reflect_on_all_associations.select{ |a| a.name == boxable_config.parent }.first
+            raise "Parent association '#{boxable_config.parent}' not found for class #{name}" unless parent_association
 
-          # Define a slug based on full_name, used for box folder name
+            associated_name = parent_association.options[:inverse_of]
+            raise "No option 'inverse_of' defined on association '#{parent_association.name}' for class #{name}" unless associated_name
+
+            {parent_method: parent_association.name, parent_association: associated_name}
+          end
+
+          # @abstract Get class folder id in Box root
+          # @return Hash
+          def self.box_folder_id_in_root
+            folder_id_key = "BOX_#{self.table_name.upcase}_FOLDER"
+            folder_id = ENV[folder_id_key]
+            raise "Cannot resolve folder '#{folder_id_key}' in ENV" unless folder_id
+
+            folder_id
+          end
+
+          # Store boxable metadata in the instance
+          cattr_accessor :boxable_config
+          self.boxable_config = Box::BoxableConfig.new(options)
+
+          # Define a slug based on full_name, used for Box folder name
           extend FriendlyId
           friendly_id :full_name, use: :slugged
 
@@ -49,7 +72,16 @@ module Box
           has_one :box_folder, as: :boxable, dependent: :destroy
         end
 
-        # Setup associations box folders as has_one
+        # Ensure model is correct on initialization, by calling critical methods
+        if boxable_config.parent
+          # Try getting class parent folder metadata, if any
+          box_parent_meta
+        else
+          # Try getting class folder in Box root, otherwise
+          box_folder_id_in_root
+        end
+
+        # Setup associations Box folders as has_one
         Box::ActsAsBoxable::Helper.boxable_associations(self).each do |a|
           box_folder_name = "box_#{a.name}_folder".to_sym
           class_eval do
@@ -58,12 +90,12 @@ module Box
         end
 
         class_eval do
-          # Build all box folders right before object creation
+          # Build all Box folders right before object creation
           before_create do |o|
             o.build_box_folder unless o.box_folder
             Box::ActsAsBoxable::Helper.boxable_associations(o.class).each do |a|
               box_folder_name = "box_#{a.name}_folder".to_sym
-              o.send("build_#{box_folder_name}", parent: :box_folder, name: a.name)
+              o.send("build_#{box_folder_name}", parent_name: :box_folder, name: a.name)
             end
           end
 
@@ -72,6 +104,28 @@ module Box
       end
 
       module InstanceMethods
+        # @abstract Get parent folder id for objects and attribute
+        # @param [Symbol] parent_name - Method to call to get parent folder (in case of attribute)
+        # @note Set above parameter to nil if object folder
+        # @return String
+        def box_parent_id(parent_name)
+          # Check whether we have an object or attribute folder
+          if parent_name
+            # Attribute folder, parent is object associated
+            send(parent_name).folder
+          else
+            # Object folder, check whether the class has a parent or not
+            if self.class.boxable_config.parent
+              # Get parent folder id
+              meta = self.class.box_parent_meta
+              parent_folder = send(meta[:parent_method]).send("box_#{meta[:parent_association]}_folder")
+              parent_folder.folder
+            else
+              # Class has no parent, get folder in box root
+              self.class.box_folder_id_in_root
+            end
+          end
+        end
       end
     end
   end
--- lib/box_folder.rb
+++ lib/box_folder.rb
@@ -5,39 +5,18 @@ class BoxFolder < ActiveRecord::Base
   after_commit :destroy_folder, on: :destroy
 
   attr_accessor :name
+  attr_accessor :parent_name
 
   def create_folder
     client = BoxToken.root.client
-    p = box_parent
-    self.parent = p.id
-    self.folder = client.create_folder(@name ? @name : boxable.slug, p).id
+    parent_id = boxable.box_parent_id(@parent_name)
+    self.parent = parent_id
+    self.folder = client.create_folder(@name ? @name : boxable.slug, parent_id).id
     self.save!
   end
 
   def destroy_folder
     client = BoxToken.root.client
     client.delete_folder(client.folder_from_id(folder))
-  end
-
-  private
-
-  def box_parent
-    client = BoxToken.root.client
-    if self.parent == 'box_folder'
-      client.folder_from_id(boxable.box_folder.folder)
-    else
-      if boxable.boxable_config.parent
-        parent_association = boxable.class.reflect_on_all_associations.select{ |a| a.name == boxable.boxable_config.parent }.first
-        raise "Association '#{boxable_config.parent}' not found for class #{boxable.class.name}" unless parent_association
-
-        associated_name = parent_association.options[:inverse_of]
-        raise "No inverse_of defined on association '#{parent_association.name}' for class #{boxable.class.name}" unless associated_name
-
-        parent_folder = boxable.send(parent_association.name.to_sym).send("box_#{associated_name}_folder".to_sym)
-        client.folder_from_id(parent_folder.folder)
-      else
-        client.folder_from_id(ENV["BOX_#{boxable.class.table_name.upcase}_FOLDER"])
-      end
-    end
   end
 end
